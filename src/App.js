1bf932029832f5bb87f1f75f2be604a9d1e4d96f
Eddie-ee <eddie@allthingsimprovement.com>
2022-10-19 13:39:43 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Here we will build new things!</p>
+        <p>What I'm reading...</p>
         <a
           className="App-link"
           href="https://reactjs.org"
