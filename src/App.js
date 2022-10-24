6164099202f12d66cb677667540e8ddb6a9e08d8
Eddie-ee <eddie@allthingsimprovement.com>
2022-10-24 15:48:49 +0200

    Commit 4.0

--- src/App.js
+++ src/App.js
@@ -6,16 +6,24 @@ function App() {
       <header className="App-header">
         <h1>Book App</h1>
         <h2>What I'm Reading</h2>
-        <div>
-          <img
-            src="https://i.postimg.cc/nzZjM1zF/dunebook.jpg"
-            alt="Dune_book"
-          />
-          <img
-            src="https://i.postimg.cc/zGF18w9S/behave-Book.jpg"
-            alt="Behave_book"
-          />
-        </div>
+        <form action="App">
+          <div>
+            <input
+              type="image"
+              src="https://i.postimg.cc/nzZjM1zF/dunebook.jpg"
+              alt="Dune_book"
+            />
+          </div>
+        </form>
+        <form action="App">
+          <div>
+            <input
+              type="image"
+              src="https://i.postimg.cc/zGF18w9S/behave-Book.jpg"
+              alt="Behave_book"
+            />
+          </div>
+        </form>
         <h2>Completed</h2>
         <div>
           <img
