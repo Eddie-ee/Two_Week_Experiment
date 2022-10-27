e637f8f2b3ba362f54f50c968ade35d9894090f2
Eddie-ee <eddie@allthingsimprovement.com>
2022-10-27 15:03:55 +0200

    Commit message

--- src/App.js
+++ src/App.js
@@ -5,39 +5,57 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Book App</h1>
-        <h2>What I'm Reading</h2>
+        <h2>Reading</h2>
         <form action="App">
           <div>
+            <label htmlFor="dune"></label>
             <input
               type="image"
               src="https://i.postimg.cc/nzZjM1zF/dunebook.jpg"
               alt="Dune_book"
+              id="dune"
             />
           </div>
         </form>
+        <br />
+
         <form action="App">
           <div>
+            <label htmlFor="behave"></label>
             <input
               type="image"
               src="https://i.postimg.cc/zGF18w9S/behave-Book.jpg"
               alt="Behave_book"
+              id="behave"
             />
           </div>
         </form>
+
         <h2>Completed</h2>
-        <div>
-          <img
-            src="https://i.postimg.cc/XNsRH79x/four-Thousand-Weeksbook.jpg"
-            alt="four_thousand_weeks_book"
-          />
-        </div>
+        <form action="App">
+          <div>
+            <label htmlFor="weeks"></label>
+            <input
+              type="image"
+              src="https://i.postimg.cc/XNsRH79x/four-Thousand-Weeksbook.jpg"
+              alt="four_thousand_weeks_book"
+              id="weeks"
+            />
+          </div>
+        </form>
+
         <h2>To Read</h2>
-        <div>
-          <img
-            src="https://i.postimg.cc/BZc1KR9x/sprint-Book.jpg"
-            alt="Sprint_book"
-          />
-        </div>
+        <form action="App">
+          <div>
+            <label htmlFor="sprint"></label>
+            <input
+              type="image"
+              src="https://i.postimg.cc/BZc1KR9x/sprint-Book.jpg"
+              alt="Sprint_book"
+              id="sprint"
+            />
+          </div>
+        </form>
       </header>
     </div>
   );
--- testing.js
+++
@@ -1,2 +0,0 @@
-const one = "Hello";
-let two = 7;
